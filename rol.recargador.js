ec6620df096c18f0807756d3378b3be256a5f14f
GCassay <geacassay@gmail.com>
2016-12-15 14:19:35 -0500

    Update rol.recargador.js

--- rol.recargador.js
+++ rol.recargador.js
@@ -1,28 +1,33 @@
 var rolRecargador = {
 
 	run: function(creep) {
-	
-		// Dejar de llevar energía al Controlador cuando alcance el level 2
-		if(creep.room.controller.level < 2){
 
-			if(creep.carry.energy == 0) { // Si el creep NO lleva carga
-				var recursos = creep.room.find(FIND_SOURCES);
-				// Se recargará con energía de la fuente si está cerca de ésta
-				if(creep.harvest(recursos[0]) == ERR_NOT_IN_RANGE) {
-					// Si no es así,  se desplazará primero hasta ella
-					creep.moveTo(recursos[0]);
-				}
-			}
-			else { // Si el creep lleva carga
+        if(creep.memory.transferir && creep.carry.energy == 0) { // Creep en Modo Transferir / Sin energía
+            creep.memory.transferir = false; // Pasar a Modo Recolección para obtener más energía
+            creep.say('transfiriendo energía');
+        }
+        if(!creep.memory.transferir && creep.carry.energy == creep.carryCapacity) { // Creep en Modo Recolección / Full energía
+            creep.memory.transferir = true; // Pasar a Modo Transferir para llevar energía a un contenedor
+            creep.say('building');
+        }
+        if(creep.memory.transferir) { // Creep en Modo Transferir / Con energía
+			// Dejar de llevar energía al Controlador cuando alcance el level 2
+			if(creep.room.controller.level < 2){
 				// Entregará la energía al Controlador si está cerca de éste
 				if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 					// Si no es así, se desplazará primero hasta él
 					creep.moveTo(creep.room.controller);
 				}
 			}
-		}
-		else{ // Si el Controlador es level 2, recargar el Contenedor
-					
+			else{
+				
+			}
+        }
+        else { // Modo Recolección 
+            var sources = creep.room.find(FIND_SOURCES); // Localizar fuente y obtener energía
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) { // Desplazarse hasta la fuente si no está cerca
+                creep.moveTo(sources[0]);
+            }
 		}
 	}
 };
