25443883ebfc3e9e3524707b4ea99771cc07b8d5
GCassay <geacassay@gmail.com>
2016-12-15 09:09:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 
+var rolRecolector = require('rol.recolector');
+var rolRecargador = require('rol.recargador');
+
+module.exports.loop = function () {
+
+    var recolectores = _.filter(Game.creeps, (creep) => creep.memory.role == 'recolector');
+    console.log('Número de Recolectores: ' + harvesters.length);
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'recolector') {
+            rolRecolector.run(creep);
+        }
+        if(creep.memory.role == 'recargador') {
+            rolRecargador.run(creep);
+        }
+    }
+}
