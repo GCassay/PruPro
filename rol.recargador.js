09dbfd72a9e1b6483f80e4aa01379f15c74844b9
GCassay <geacassay@gmail.com>
2016-12-15 12:44:24 -0500

    Update rol.recargador.js

--- rol.recargador.js
+++ rol.recargador.js
@@ -1,17 +1,29 @@
 var rolRecargador = {
 
-    run: function(minion) {
-	    if(minion.carry.energy == 0) {
-            var recursos = minion.room.find(FIND_SOURCES);
-            if(minion.harvest(recursos[0]) == ERR_NOT_IN_RANGE) {
-                minion.moveTo(recursos[0]);
-            }
-        }
-        else {
-            if(minion.upgradeController(minion.room.controller) == ERR_NOT_IN_RANGE) {
-                minion.moveTo(minion.room.controller);
-            }
-        }
+	run: function(minion) {
+	
+		// Dejar de llevar energía al Controlador cuando alcance el level 2
+		if(minion.room.controller.level < 2){
+
+			if(minion.carry.energy == 0) { // Si el creep NO lleva carga
+				var recursos = minion.room.find(FIND_SOURCES);
+				// Se recargará con energía de la fuente si está cerca de ésta
+				if(minion.harvest(recursos[0]) == ERR_NOT_IN_RANGE) {
+					// Si no es así,  se desplazará primero hasta ella
+					minion.moveTo(recursos[0]);
+				}
+			}
+			else { // Si el creep lleva carga
+				// Entregará la energía al Controlador si está cerca de éste
+				if(minion.upgradeController(minion.room.controller) == ERR_NOT_IN_RANGE) {
+					// Si no es así, se desplazará primero hasta él
+					minion.moveTo(minion.room.controller);
+				}
+			}
+		}
+		else{ // Si el Controlador es level 2, recargar el Contenedor
+					
+		}
 	}
 };
 
