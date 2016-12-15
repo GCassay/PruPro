6e4a167f6c84fb92dcb3272d6153a4a0bdd7100f
GCassay <geacassay@gmail.com>
2016-12-15 18:10:28 -0500

    Update rol.recolector.js

--- rol.recolector.js
+++ rol.recolector.js
@@ -1,5 +1,5 @@
 var rolRecolector = {
-    // Función para recoger energía
+    // Recoger energía para el Spawn (si tiene capacidad)
     run: function(creep) {
         // Si el recolector lleva menos energía de la que puede transportar, intenta obtener más
         if(creep.carry.energy < creep.carryCapacity) {
@@ -9,7 +9,7 @@ var rolRecolector = {
                 creep.moveTo(recursos[0]);
             }
         }
-        // Si el recolector está lleno y el Spawn Centro tiene aún capacidad, dirigirse hasta él y transferirla
+        // Si el recolector está lleno y el Spawn Central tiene aún capacidad, dirigirse hasta él y transferirla
         else if(Game.spawns['Central'].energy < Game.spawns['Central'].energyCapacity) {
             if(creep.transfer(Game.spawns['Central'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.spawns['Central']);
