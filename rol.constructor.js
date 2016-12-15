53ca32ef4ab0f537bdedb1f565212c1e848441e0
GCassay <geacassay@gmail.com>
2016-12-15 14:32:58 -0500

    Update rol.constructor.js

--- rol.constructor.js
+++ rol.constructor.js
@@ -2,15 +2,15 @@ var rolConstructor = {
 
     run: function(creep) {
 
-        if(creep.memory.building && creep.carry.energy == 0) { // Creep en Modo Construcción / Sin energía
-            creep.memory.building = false; // Pasar a Modo Recolección para obtener más energía
-            creep.say('recolectando');
+        if(creep.memory.construir && creep.carry.energy == 0) { // Creep en Modo Construcción / Sin energía
+            creep.memory.construir = false; // Pasar a Modo Recolección para obtener más energía
+            creep.say('Recolectando');
         }
-        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) { // Creep en Modo Recolección / Full energía
-            creep.memory.building = true; // Pasar a Modo Construcción para comenzar a construir
-            creep.say('construyendo');
+        if(!creep.memory.construir && creep.carry.energy == creep.carryCapacity) { // Creep en Modo Recolección / Full energía
+            creep.memory.construir = true; // Pasar a Modo Construcción para comenzar a construir
+            creep.say('Construyendo');
         }
-        if(creep.memory.building) { // Creep en Modo Construcción / Con energía
+        if(creep.memory.construir) { // Creep en Modo Construcción / Con energía
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES); // Localizar punto de construcción y construir
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) { // Desplazarse hasta el punto si no está cerca
