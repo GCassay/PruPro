8a25da58890d0188cb095e7609eacb770da13a10
GCassay <geacassay@gmail.com>
2016-12-15 11:42:31 -0500

    Update rol.recargador.js

--- rol.recargador.js
+++ rol.recargador.js
@@ -1,1 +1,18 @@
+var rolRecargador = {
 
+    run: function(minion) {
+	    if(minion.carry.energy == 0) {
+            var recursos = minion.room.find(FIND_SOURCES);
+            if(minion.harvest(recursos[0]) == ERR_NOT_IN_RANGE) {
+                minion.moveTo(recursos[0]);
+            }
+        }
+        else {
+            if(minion.upgradeController(minion.room.controller) == ERR_NOT_IN_RANGE) {
+                minion.moveTo(minion.room.controller);
+            }
+        }
+	}
+};
+
+module.exports = rolRecargador;
