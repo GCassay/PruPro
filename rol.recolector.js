4fa1fa3ba1f2660158c545474315693ff8b888b0
GCassay <geacassay@gmail.com>
2016-12-15 11:09:31 -0500

    Update rol.recolector.js

--- rol.recolector.js
+++ rol.recolector.js
@@ -5,8 +5,8 @@ var rolRecolector = {
         if(creep.carry.energy < creep.carryCapacity) {
             var recursos = creep.room.find(FIND_SOURCES);
             // Si no está en el rango de una fuente, desplazarse hasta una una
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+            if(creep.harvest(recursos[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(recursos[0]);
             }
         }
         // Si el recolector está lleno y el Spawn Centro tiene aún capacidad, dirigirse hasta él y transferirla
